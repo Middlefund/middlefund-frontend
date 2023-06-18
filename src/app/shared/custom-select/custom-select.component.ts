81000309059f57180d7e9783d3a4e9acb364b068
Bilal-Abubakari <bilal.abubakari@amalitech.com>
2023-06-18 21:51:15 +0000

    start profile form

---
+++ src/app/shared/custom-select/custom-select.component.ts
@@ -0,0 +1,27 @@
+import {Component, Input} from '@angular/core';
+import {FormControl} from "@angular/forms";
+
+@Component({
+  selector: 'app-custom-select',
+  templateUrl: './custom-select.component.html',
+  styleUrls: ['./custom-select.component.css']
+})
+export class CustomSelectComponent {
+  @Input() label: string = '';
+  @Input() for: string = '';
+  @Input() type: string = ''
+  @Input() placeholder: string = ''
+  @Input() options:{ name: string, value: any }[] = []
+  @Input() control = new FormControl();
+  @Input() loadingOptions = false;
+
+  errorMessages: Record<string, string> = {
+    required: "This field is required.",
+    pattern: "You input is invalid.",
+    email: "Provide a valid email",
+    passwordMismatch: "Password must be a minimum of 8 characters, include uppercase, number and special character.",
+    invalidEmail: "Please provide a valid email",
+    invalidFullName: "Provide a name in this format: Firstname Lastname",
+    invalidConfirmPassword: "Passwords do not match"
+  }
+}
