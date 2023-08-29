83757b4057158161cef24aacd3893e3854c0fc89
Bilal-Abubakari <bilal.abubakari@amalitech.com>
2023-08-29 08:33:20 +0000

    incorporating your company

--- src/app/platinum/business-description/business-description.component.ts
+++ src/app/platinum/business-description/business-description.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
+import {PitchSubmissionService} from "../../pitch-submission/pitch-submission.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-business-description',
   templateUrl: './business-description.component.html',
   styleUrls: ['./business-description.component.css']
 })
-export class BusinessDescriptionComponent {
+export class BusinessDescriptionComponent implements OnInit {
+  loadingIndustries = false;
+  industries: Array<{name: string, value: string}> = []
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+              private pitchService: PitchSubmissionService,
+              private toast: ToastrService) {
+  }
+
+  ngOnInit() {
+    this.getAllIndustries()
   }
 
   businessDescription = this.fb.group({
@@ -17,6 +27,21 @@ export class BusinessDescriptionComponent {
   })
 
   onSubmitBusinessDescription() {
+  }
 
+  getAllIndustries() {
+    this.loadingIndustries = true;
+    this.pitchService.getIndustries().subscribe({
+      next: value => {
+        value.data.map((industry: {name: string}) => {
+          this.industries.push({name: industry.name, value: industry.name})
+        })
+        this.loadingIndustries = false;
+      },
+      error: error => {
+        this.loadingIndustries = false;
+        this.toast.error(error.error.message || "Oops! Server error")
+      }
+    })
   }
 }
