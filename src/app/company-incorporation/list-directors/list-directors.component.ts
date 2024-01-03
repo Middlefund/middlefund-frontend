df1c8369a8c121bf7cd7d8adda3d603725876d72
= <=>
2024-01-03 07:32:01 +0000

    feat: list of directors and submission

--- src/app/company-incorporation/list-directors/list-directors.component.ts
+++ src/app/company-incorporation/list-directors/list-directors.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { capitalizeWords } from '../../utility/capitalizeEachWord';
 import { IProprietorDirector } from '../../models/companyIncorporation.interface';
 import { CompanyIncorporationService } from '../company-incorporation.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { defaultServerError } from '../../utility/constants';
 import { ToastrService } from 'ngx-toastr';
 
@@ -15,12 +15,15 @@ export class ListDirectorsComponent implements OnInit {
   protected readonly capitalizeWords = capitalizeWords;
   directors: IProprietorDirector[] = [];
   isLoading: boolean = false;
+  isSubmitting: boolean = false;
   id: string = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  status: string = 'incomplete';
 
   constructor(
     private companyIncorporationService: CompanyIncorporationService,
     private activatedRoute: ActivatedRoute,
     private toast: ToastrService,
+    private router: Router,
   ) {}
 
   ngOnInit() {
@@ -35,9 +38,29 @@ export class ListDirectorsComponent implements OnInit {
         next: value => {
           this.directors = value.data;
           this.isLoading = false;
+          this.status = value.status;
         },
         error: err => {
           this.isLoading = false;
+          this.toast.error(err.error?.message || defaultServerError);
+        },
+      });
+  }
+
+  submit() {
+    this.isSubmitting = true;
+    this.companyIncorporationService
+      .submitDirectors(
+        this.activatedRoute.snapshot.paramMap.get('id') as string,
+      )
+      .subscribe({
+        next: () => {
+          this.isSubmitting = false;
+          this.router.navigateByUrl('../../home');
+          this.status = 'awaiting review';
+        },
+        error: err => {
+          this.isSubmitting = false;
           this.toast.error(err.error?.message || defaultServerError);
         },
       });
